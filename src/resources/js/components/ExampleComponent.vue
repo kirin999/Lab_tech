3e78b4928b4cd9f5cee529c789bc3570a392cdb1
kirin <kirin659@gmail.com>
2023-01-28 06:31:08 +0900

    Lab_techスタート

---
+++ src/resources/js/components/ExampleComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    aaaaaaaaaaaaaaaa
+</template>
+
+<!-- <script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script> -->
